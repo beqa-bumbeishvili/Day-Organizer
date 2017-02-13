29fa4381fb0ce30729853f3e66feb55be99f078b
beqa-bumbeishvili <bumbeishvili.beqa@gmail.com>
2017-02-13 22:42:24 +0400

    initial commit

---
+++ DayOrganizer.Domain/Abstract/ITaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayOrganizer.Domain.Abstract
+{
+    public interface ITaskRepository
+    {
+        IEnumerable<Task>Tasks { get; set; }
+    }
+}
---
+++ DayOrganizer.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DayOrganizer.Web.MvcApplication" Language="C#" %>
